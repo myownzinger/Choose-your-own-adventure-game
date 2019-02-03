aef048ba47a0cb1a44022e716d5d0aeeb4e5a0e6
ameinzinger <45666145+ameinzinger@users.noreply.github.com>
2019-02-03 12:45:45 -0500

    Additions to game

--- Game.js
+++ Game.js
@@ -10,7 +10,11 @@ const GameState = Object.freeze({
     STAY_OR_GO: Symbol("stayOrGo"),
     PLAY_AGAIN: Symbol("playAgain"),
     QUESTIONS: Symbol("questions"),
-    END: Symbol("end")
+    END: Symbol("end"),
+    ROCK_CAKES: Symbol("rockCakes"),
+    GRINGOTTS: Symbol("gringotts"),
+    WHERE_TO_FIRST: Symbol("whereToFirst"),
+    REAL_END: Symbol("realEnd")
 });
 
 export default class Game{
@@ -20,7 +24,7 @@ export default class Game{
         this.object = objList[Math.floor(Math.random()*objList.length)];
         this.gameNum=0;
     }
-    // boolean function returns true if users guess is in chosen object's property list
+    // boolean function returns true when users guess is in chosen object's property list
     guessCorrect(sInput){
         var result = false;
         this.object.forEach(checkValue)
@@ -68,7 +72,7 @@ export default class Game{
                 }
                 break;
             case GameState.WRITE_BACK:
-                if (sInput.toLowerCase().match("uncle")) {
+                if(sInput.toLowerCase().match("uncle")) {
                     sReply = "You're sorry you ever asked, your uncle yells so much his face turns a deep shade of purple for lack of breath. He then proceeds to lock you in your cupboard for a week. Type y to continue";
                     this.stateCur = GameState.CRAZY_UNCLE
                 }
@@ -78,7 +82,7 @@ export default class Game{
                 }
                 break;
             case GameState.CRAZY_UNCLE:
-                 if (sInput.toLowerCase().match("y")){
+                 if(sInput.toLowerCase().match("y")){
                     sReply = "Letters continue to arrive through the following week, more frequently than ever, and in more bizarre ways than you thought possible (some even arrived balled up inside your Aunt's freshly bought eggs. Finally your uncle seems to snap. With his vein popping from his forehead he declares that the whole family is 'Going away' and that you have five minutes to pack. A day later, you're in a remote cabin. It's almost your birthday. As the clock strikes midnight, a large Man breaks into the cottage and tells you you're a wizard and should come with him. Do you go with him or stay with your Aunt and Uncle?";
                     this.stateCur = GameState.STAY_OR_GO
                 }
@@ -88,7 +92,7 @@ export default class Game{
                 }
                 break;
             case GameState.STAY_OR_GO:
-                if (sInput.toLowerCase().match("go")){
+                if(sInput.toLowerCase().match("go")){
                     sReply="The large man tells you all about your parents and the Wizarding world on your boat ride away from the island, and as you speed forward in the boat, you can't help but feel like your story is just beginning. Do you want to continue, play again, or stop?";
                     this.stateCur = GameState.PLAY_AGAIN;
                 }
@@ -98,12 +102,13 @@ export default class Game{
                 }
                 break;
             case GameState.PLAY_AGAIN:
-                if (sInput.toLowerCase().match("play")||sInput.toLowerCase().match("again")||sInput.toLowerCase().match("start")){
+                if(sInput.toLowerCase().match("play")||sInput.toLowerCase().match("again")||sInput.toLowerCase().match("start")||sInput.toLowerCase().match("yes")){
                     sReply = "Would you like to play HPAdventure or 20Questions?";
                     this.stateCur = GameState.CHOOSE_GAME;
                 }
-                else if (sInput.toLowerCase().match("continue")){
-                    // START HERE
+                else if(sInput.toLowerCase().match("continue")){
+                    sReply = "When you get to shore, the giant asks if you'd like to stop and eat some of the rock cakes he brought before you go to buy your things. Do you say yes or no?";
+                    this.stateCur = GameState.ROCK_CAKES;
                 }
                 else {
                     sReply = "Okay, have a nice day. Type anything to play again."
@@ -125,7 +130,7 @@ export default class Game{
                     this.object = objList[Math.floor(Math.random(this.gameNum)*objList.length)];
                     this.stateCur =GameState.PLAY_AGAIN;
                 }
-                else if (this.guessCorrect(sInput)){
+                else if(this.guessCorrect(sInput)){
                     this.numQuestions-=1;
                     sReply="Yes! "+this.numQuestions+" questions left.";
                     this.stateCur = GameState.QUESTIONS;
@@ -140,6 +145,57 @@ export default class Game{
                 if(sInput){
                     sReply = "Welcome back! Would you like to play HPAdventure or 20Questions?"
                     this.stateCur = GameState.CHOOSE_GAME;
+                }
+                break;
+            case GameState.ROCK_CAKES:
+                if(sInput.toLowerCase().match("yes")){
+                    sReply = "You take the small package Hagrid (you've learned his name now) gives you. You unwrap it and take a bite, immediately regretting your decision having chipped a tooth. You smile at Hagrid but throw the rest in a bush when he's not looking. Hagrid asks if you're ready to buy your things now. Do you say yes or no?";
+                    this.stateCur = GameState.DIAGONALLY;                
+                }
+                else {
+                    sReply = "The giant Hagrid says he'd still like to stop for a bite, so he takes you to a pub called the leaky cauldron that the muggles outside don't seem to notice. As you finish your soup he asks if you're ready to move on. Are you?";
+                    this.stateCur = GameState.DIAGONALLY;
+                }
+                break;
+            case GameState.DIAGONALLY:
+                if(sInput.toLowerCase().match("yes")){
+                    sReply = "Hagrid takes you to the back of the leaky cauldron, where  he taps a brick on the back wall three times. Before your eyes an arch way appears leading into a small winding cobbled street full of shops! Do you sprint ahead or ask Hagrid where to go?";
+                    this.stateCur = GameState.GRINGOTTS;
+                }else {
+                    sReply = "Hagrid twiddles his thumbs and begins to look impatient. He leans forward and asks if you're ready to go now. Are you?"
+                }
+                break;
+            case GameState.GRINGOTTS:
+                if(sInput.toLowerCase().match("sprint")||sInput.toLowerCase().match("ahead")||sInput.toLowerCase().match("run")){
+                    sReply = "You sprint into the street but barely get anywhere before a large hand grabs you by the shoulder, stopping you in your tracks. 'Where do you think you're going?! We have to go get yer money first! You go to the bank and take out more gold than you've ever seen in your life (but it's just a fraction of what you have). Hagrid asks if you'd like to first get your robes, your books, or your wand."
+                    this.stateCur = GameState.WHERE_TO_FIRST;
+                }
+                else {
+                    sReply = "You ask Hagrid where to go first, but he's already bounding down the street so you run to catch up. He takes you to the bank and helps you take out more money than you even knew existed! With your new sack of gold in hand, he asks you what you'd like to get first, your robes, your books or your wand.";
+                    this.stateCur = GameState.WHERE_TO_FIRST;
+                }
+                break;
+            case GameState.WHERE_TO_FIRST:
+                if(sInput.toLowerCase().match("robe")){
+                    sReply = "Hagrid offers to grab your books while you try on your robes. You meet a straw haired kid while you're in there, but he doesn't seem very nice. Hagrid comes back with your books and asks if you're ready to go get your wand. Do you say yes?";
+                    this.stateCur = GameState.REAL_END;
+                }
+                else if(sInput.toLowerCase().match("books")){
+                    sReply = "Hagrid says it might be best if he grabs your books for you while you get your robes fitted. You meet a pinch-faced straw haired boy who rubs you the wrong way. Hagrid comes back and asks if you're ready to get your wand. Do you say yes?";
+                    this.stateCur = GameState.REAL_END;
+                }
+                else {
+                    sReply = "You go into the wand shop while Hagrid goes to run an errand. An old man named Ollivander comes out and keeps putting wands in your hand until one finally sends sparks into the air. When Hagrid comes back, he's holding a cage containing a large snowy owl. He says its for your birthday, and asks if you're ready to get your robes. Do you say yes?";
+                    this.stateCur = GameState.REAL_END;
+                }
+                break;
+            case GameState.REAL_END:
+                if(sInput.toLowerCase().match("yes")||sInput.toLowerCase().match("ye")){
+                    sReply = "You finish getting your things (with Hagrid's help) and leave Diagon Ally with a chest full of your supplies for school and your new owl Hedwig. Hagrid drops you at King's Cross Station, and you say farewell, sure that you'll meet again soon. Would you like to play again?";
+                    this.stateCur = GameState.PLAY_AGAIN;
+                }
+                else {
+                    sReply = "Hagrid looks impatient now. He asks you one more time if you're ready to get the rest of your things."
                 }
                 break;
         }
